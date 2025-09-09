c94a72aa03f7d37abfaf7d4ce346e2fd1175f486
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2025-09-09 09:58:51 +0200

    chnage resolution

--- src/config/screens.js
+++ src/config/screens.js
@@ -3,7 +3,7 @@ export const screens = {
 	'2xs': '320px',
 	xs: '375px', // iPhone SE, minis
 	'xs+': '390px', // iPhone standard models
-	'sm-': '440px', // Large phones
+	'sm-': '480px', // Large phones
 	sm: '640px',
 	md: '768px',
 	lg: '1024px',
